22f68567aff68554336133a7422fd7f2cba8e155
Timpan42 <tim.fagerdal@elev.ga.ntig.se>
2023-04-25 16:23:58 +0200

    ✨ kan röra spelare

--- public/js/game.js
+++ public/js/game.js
@@ -71,7 +71,7 @@ function update() {
     } else{
       this.ship.setAcceleration(0);
     }
-        this.physics.world.wrap(this.ship, 5);
+      this.physics.world.wrap(this.ship, 5);
   } 
 
  }
