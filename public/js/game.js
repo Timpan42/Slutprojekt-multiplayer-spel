048cc367a00fdd0128675343efdec2de50664802
Timpan42 <tim.fagerdal@elev.ga.ntig.se>
2023-04-25 15:37:25 +0200

    ✨ kan ha flera spelare

--- public/js/game.js
+++ public/js/game.js
@@ -21,6 +21,7 @@ var game = new Phaser.Game(config);
 function preload() {
 
   this.load.image('ship', 'assets/playerShip1_red.png');
+  this.load.image('otherPlayer', 'assets/playerShip3_blue.png');
 
 }
 
@@ -28,14 +29,29 @@ function create() {
 
 var self  = this;
 this.socket = io();
+this.otherPlayers = this.physics.add.group();
 this.socket.on('currentPlayers', function (players){
   Object.keys(players).forEach(function (id){
     if(players[id].playerId === self.socket.id){
       addPlayer(self, players[id]);
+    } else {
+      addOtherPlayers(self, players[id]);
     }
   });
 });
-  
+
+this.socket.on('newPlayer', function (playerInfo){
+  addOtherPlayers(self, players[id]);
+});
+
+this.socket.on('disconnect', function (playerId){
+  self.otherPlayers.getChildren().forEach(function (otherPlayers){
+    if(playerId === otherPlayers.playerId){
+      otherPlayers.destroy();
+    }
+  });
+});
+
 }
 
 function update() {
@@ -44,7 +60,7 @@ function update() {
  }
 
 function addPlayer(self, playerInfo){
-self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53,40);
+  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53,40);
   if(playerInfo.team === 'blue'){
     self.ship.setTint(0x0000ff);
   } else {
@@ -53,5 +69,15 @@ self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin
   self.ship.setDrag(100,100);
   self.ship.setAngularDrag(100);
   self.ship.setMaxVelocity(200);
-  
  }
+
+ function addOtherPlayers(self, playerInfo){
+  const otherPlayers = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53,40);
+  if(playerInfo.team === 'blue'){
+    otherPlayers.setTint(0x0000ff);
+  } else {
+    otherPlayers.setTint(0xff0000);
+  }
+  otherPlayers.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayers);
+}
